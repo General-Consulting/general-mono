49549a2cc92238c9363df2eb63443e4d00872baa
geoff <geoff@generalconsulting.io>
2024-02-14 22:33:22 -0500

    ok

--- flake.nix
+++ flake.nix
@@ -17,6 +17,8 @@
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+
   };
 
   outputs = { self, nixpkgs, flake-utils, devshell, flake-parts, mission-control, flake-root, treefmt-nix, ... }@inputs:
@@ -31,7 +33,68 @@
 
 
 
-      perSystem = { pkgs, system, self', config, inputs', lib, ... }: {
+      perSystem = { pkgs, system, self', config, inputs', lib, ... }: 
+
+       let 
+      nextUser = pkgs.runCommand "user" {} ''
+        mkdir -p $out/etc
+        echo "nextjs:x:1000:1000:nextjs:/home/nextjs:/bin/false" > $out/etc/passwd
+        echo "nextjs:x:1000:" > $out/etc/group
+        echo "nextjs:!:1::::::" > $out/etc/shadow
+      '';
+
+      dockerImage = pkgs.dockerTools.buildImage {
+        name = "app";
+        tag = "latest";
+        copyToRoot = [
+          # Uncomment the coreutils and bash if you want to be able to use a shell environment
+          # inside the container.
+          #pkgs.coreutils
+          #pkgs.bash
+          nextUser
+          pkgs.nodejs-18_x
+          pdf-app
+        ];
+        config = {
+          Cmd = [ "node" "server.js" ];
+          User = "nextjs:nextjs";
+          Env = [ "NEXT_TELEMETRY_DISABLED=1" ];
+          ExposedPorts = {
+              "3000/tcp" = {};
+          };
+          WorkingDir = "/app";
+        };
+      };
+
+
+
+        pdf-app = pkgs.buildNpmPackage {
+          name = "dulcet23";
+
+          buildInputs = with pkgs; [
+            nodejs-18_x
+          ];
+
+          src = ./nextjs/dulcet23/.;
+
+          npmDepsHash = "sha256-3IyRkzPckXsmeDkZew3KvkviE7YwOd5hyDNS7Hw2iAs=";
+
+          installPhase = ''
+            mkdir -p $out
+            cp .next/* $out
+          '';
+        };
+        in 
+      {
+
+
+        
+        packages = {
+            pdf-app = pdf-app;
+            dockerImage = dockerImage;
+            default = pdf-app;
+          };
+    
 
         treefmt.config = {
           inherit (config.flake-root) projectRootFile;
--- nextjs/dulcet23/app/household/@content/members/MembersTable.tsx
+++ nextjs/dulcet23/app/household/@content/members/MembersTable.tsx
@@ -1,42 +1,33 @@
-'use client'
+"use client";
 
-import { Fragment, useState } from "react"
-
-import FieldFactory from "@/components/FieldFactory"
-import FormModal from "@/components/FormModal"
-import { 
-  TableContainer,
-  TableTitle,
-  Table,
-} from "@/components/Table"
-import HighlightButton from "@/components/HighlightButton"
-import MemberFields from "./MemberFields"
-import useMembersSubset from "@/store/useMembersSubset"
-import { FieldData } from '@/types'
-import getDefaultValues from "@/utils/getDefaultValues"
-
-
+import FormModal from "@/components/FormModal";
+import { TableContainer, TableTitle, Table } from "@/components/Table";
+import HighlightButton from "@/components/HighlightButton";
+import useMembersSubset from "@/store/useMembersSubset";
+import { FieldData } from "@/types";
+import getDefaultValues from "@/utils/getDefaultValues";
+import { useState } from "react";
 
 const forDevFieldsData: FieldData[] = [
   {
-    name: 'name.firstName',
-    label: 'First Name',
-    component: 'Input' 
+    name: "name.firstName",
+    label: "First Name",
+    component: "Input",
   },
   {
-    name: 'name.lastName',
-    label: 'Last Name',
-    component: 'Input' 
-  }
-]
+    name: "name.lastName",
+    label: "Last Name",
+    component: "Input",
+  },
+];
 
 const MembersTable = () => {
-  const [ openModal, setOpenModal ] = useState('')
-  const membersSubset = useMembersSubset()
+  const [openModal, setOpenModal] = useState("");
+  const membersSubset = useMembersSubset();
 
-  const defaultValues = getDefaultValues(forDevFieldsData)
+  const defaultValues = getDefaultValues(forDevFieldsData);
 
-  console.log('membersSubset', membersSubset)
+  console.log("membersSubset", membersSubset);
 
   return (
     <TableContainer>
@@ -46,27 +37,27 @@ const MembersTable = () => {
       />
       <Table
         tableData={membersSubset}
-        onEdit={() => console.log('Edit!')}
-        onDelete={() => console.log('Delete!')}
+        onEdit={() => console.log("Edit!")}
+        onDelete={() => console.log("Delete!")}
       />
       <HighlightButton
         onClick={() => {
-          console.log('Clicked add member, add functionality :)')
-        }} 
+          console.log("Clicked add member, add functionality :)");
+        }}
       >
         Add
       </HighlightButton>
       <FormModal
         title="Edit member"
         isOpen={true}
-        onCancel={() => console.log('Cancelled!')}
-        onSave={() => console.log('Saved!  Haha, not really...')}
+        onCancel={() => console.log("Cancelled!")}
+        onSave={() => console.log("Saved!  Haha, not really...")}
         defaultValues={defaultValues}
       >
-      hi
+        hi
       </FormModal>
     </TableContainer>
-  )
-}
+  );
+};
 
-export default MembersTable
+export default MembersTable;
