0897b3d5d540fc3f86f029906c8a1a3e935ebac1
geoff <geoff@generalconsulting.io>
2024-02-17 18:36:17 -0500

    grab

--- nextjs/dulcet23/types/collection.types.ts
+++ nextjs/dulcet23/types/collection.types.ts
@@ -3,10 +3,75 @@ import {
   Income
 } from '@/types'
 
-export enum Collection {
+export const Collections {
   Assets = 'assets',
   Incomes = 'incomes',
 }
+
+const collectionType = {
+  income: typeof Collection<Income>,
+  asset: typeof Collection<Asset>
+}
+
+interface Collection<T> {
+  type: typeof T
+  addItem(key: string, item: T) : void
+  removeItem(key: string) : void
+  items()
+}
+
+
+class IncomeCollection implements Collection<Income> {
+  store: IncomeStore
+
+  addItem(key: string, item: Income){
+    store.addIncomeItem(item);
+  }
+
+  items(){
+    return store.income.listAll();
+  }
+
+}
+
+export interface Income {
+  id: string;
+  sourceName: string;
+  sourceAddress: Address;
+  incomeType: 'Employment' | 'Other';
+  frequency: 'Weekly' | 'Every two weeks' | 'Twice a month' | 'Monthly' | 'Quarterly' | 'Yearly'
+  amount: number; 
+}
+
+export interface Asset {
+  id: string;
+  assetName: string;
+  assetType: string;
+  value: number;
+}
+
+
+// Adjust HouseholdState interface to use these specific types
+export interface HouseholdState {
+  household: Household;
+  addMember: (newMemberData: Member) => void;
+  editMember: ({ memberId, data }: EditMemberArgs) => void;
+  deleteMember: (memberId: string) => void;
+  addCollection: (params: Collection<T>) => void;
+  getCollections: () => Collection<T>[];
+  getCollection: (collectionType: string) => Collection<T>[];
+}
+
+class HouseholdState {
+  household: HouseholdStore;
+  addMember(newMemberData: Member) => {
+    household.members.push(newMemberData)
+  }
+
+  
+  ...
+}
+houseState.getCollection(Income'.addItem(someIncome)
 
 // What collection might be possibly required?
 // answered in CollectionTable
